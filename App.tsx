eea7a8bd822a5aabb5c370ba90997bc47e10e382
shivank111chauhan <shivank111chauhan@gmail.com>
2023-02-14 17:15:06 +0530

    Api calling

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import Home from './src/Home';
 import Dashboard from './src/Dashboard';
 import ListComponent from './src/ListComponent';
 import { Test } from './src/Test';
+import ApiCalling from './src/ApiCalling';
 
 const App = () => {
   const stack = createNativeStackNavigator();
@@ -18,11 +19,12 @@ const App = () => {
 
     //Using Navigation to reach Home page...............
     <NavigationContainer>
-      <stack.Navigator initialRouteName="Dashboard">
+      <stack.Navigator initialRouteName="Api">
         <stack.Screen name='Dashboard' component={Dashboard}/>
         <stack.Screen name="Home" component={Home} />
         <stack.Screen name='Test' component={Test}/>
         <stack.Screen name='todo' component={ListComponent}/>
+        <stack.Screen name='Api' component={ApiCalling}/>
         </stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/ApiCalling.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const ApiCalling = () => {
+    const apiUrl = 'https://reactnative.dev/movies.json';
+    const [isloading, setIsloading] = useState(true);
+    const [Data, setData] = useState([]);
+
+    const getData = async() =>{
+        try{const response = await fetch(apiUrl);
+        const json = await response.json();
+        setData(json.movies);
+    }catch(error){
+            console.log(error);
+        }finally{setIsloading(false)}
+    }
+    
+    useEffect(() => {
+        getData();
+    },[])
+
+    return (
+        <View style={styles.container}>
+            {isloading ? <ActivityIndicator /> :
+            <FlatList
+                data={Data}
+                keyExtractor={({ id }) => id}
+                renderItem={({ item }) => (
+                    <Text>{item.title},{item.releaseYear}</Text>
+                )}
+            />}
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: 'black'
+    }
+})
+
+export default ApiCalling;
+
+
